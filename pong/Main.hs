3bb6402504f0e7526d869287f70a9a4a35b7be2e
Nathanael Gutierrez <nathanaelg16@gmail.com>
2020-05-30 23:49:48 -0400

    trying to enable websocket support to client

--- pong/Main.hs
+++ pong/Main.hs
@@ -3,6 +3,11 @@ module Main(main, PongGame(..), render, initialState) where
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game 
+import Network.Socket
+import System.IO
+import Control.Concurrent
+import Control.Monad (when)
+import Control.Monad.Fix (fix)
 
 width, height, offset :: Int
 width = 300
@@ -34,9 +39,33 @@ paddleWidth = 26
 
 type Radius = Float
 type Position = (Float, Float)
+type Pong = (Float, -- ^ Ball x velocity
+  Float, -- ^ Ball y velocity
+  Float) -- ^ Player paddle position
 
 main :: IO ()
-main = play window background fps initialState render handleKeys update
+main = do
+  play window background fps initialState render handleKeys update 
+  sock <- socket AF_INET Stream 0
+  setSocketOption sock ReuseAddr 1
+  bind sock (SockAddrInet 4242 iNADDR_ANY)
+  listen sock 2
+  chan <- newChan
+  _ <- forkIO $ fix $ \loop -> do
+    (_, _, _) <- readChan chan
+    loop
+  mainLoop sock chan
+
+mainLoop :: Socket -> Chan Pong -> IO ()
+mainLoop sock chan = do
+  conn <- accept sock
+  forkIO (runConn conn chan)
+  mainLoop sock chan
+
+runConn :: (Socket, SockAddr) -> Chan Pong -> IO ()
+runConn (sock, _) chan = do
+
+-- play window background fps initialState render handleKeys update
 -- | Update the game by moving the ball
 -- Ignore the ViewPort argument
 update :: Float -> PongGame -> PongGame
